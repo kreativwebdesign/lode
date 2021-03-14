847f0da638c43d82ac2546e4406610c94ca03cc1
Simon Stucki <simon_stucki@hotmail.com>
2021-03-14 20:28:17 +0100

    Extend benchmark logging
    Log fps for demo application.

--- benchmark/index.js
+++ benchmark/index.js
@@ -2,47 +2,48 @@ const fs = require('fs')
 const puppeteer = require('puppeteer')
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
+const { waitFor, startLogGroup, createFolderIfNotExist } = require('./helpers')
 
 const argv = yargs(hideBin(process.argv)).argv
 
-const startTimer = () => {
-  const start = process.hrtime.bigint()
-  return () => {
-    const end = process.hrtime.bigint(start)
-    return end - start
-  }
-}
-
-const waitFor = async (ms) => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, ms)
-  })
-}
-
-const startLogGroup = (group) => {
-  const stopTimer = startTimer()
-  console.log(group + ' started')
-
-  return () => {
-    const diff = stopTimer()
-    console.log(group + ' ended, duration: ' + diff + 'ns')
-  }
-}
-
 const main = async () => {
   const TEMP_FOLDER = 'tmp/'
-  if (!fs.existsSync(TEMP_FOLDER)) {
-    fs.mkdirSync(TEMP_FOLDER)
-  }
+  createFolderIfNotExist(TEMP_FOLDER)
   console.log('start benchmark')
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  page.on('console', (message) =>
-    console.info('web-console: ' + message.text())
-  )
+  const reportGroups = {
+    fps: [],
+  }
+
+  const report = (log) => {
+    const namespaces = log.split('::')
+    if (namespaces.length !== 4) {
+      console.error(
+        'received non matching log message, must follow ::benchmark::namepsace::value semantics',
+        log
+      )
+      return
+    }
+    const action = namespaces[2]
+    switch (action) {
+      case 'fps':
+        reportGroups.fps.push(parseInt(namespaces[3]))
+        break
+      default:
+        console.error('unknown action', action, log)
+    }
+  }
+
+  page.on('console', (message) => {
+    const log = message.text()
+    if (log.includes('::benchmark::')) {
+      report(log)
+    } else {
+      console.info('web-console: ' + message.text())
+    }
+  })
 
   let finishGroup = startLogGroup('load page')
   await page.goto(`http://localhost:8080?${argv.o ? 'optimize' : ''}`)
@@ -65,9 +66,10 @@ const main = async () => {
   console.log('start analyzer')
 
   const trace = JSON.parse(fs.readFileSync(`${TEMP_FOLDER}trace.json`, 'utf8'))
-  let totalTime = 0
 
   const events = trace.traceEvents ? trace.traceEvents : trace
+
+  let totalTime = 0
   let numberOfEvents = 0
   events.forEach((event) => {
     if (event.name.toLowerCase().includes('gpu')) {
@@ -81,6 +83,7 @@ const main = async () => {
   })
   console.log('gpu duration: ' + totalTime)
   console.log('total tasks: ' + numberOfEvents)
+  console.log('fps: ' + reportGroups.fps)
 }
 
 main()
