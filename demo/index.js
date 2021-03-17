55943e327bc3114da59d524822454285bb53ff79
Marc <marc.berli@windowslive.com>
2021-03-17 21:58:36 +0100

    Remove camera roation

--- demo/index.js
+++ demo/index.js
@@ -35,10 +35,6 @@ const createScene = function () {
     function (scene) {
       // Create a default arc rotate camera and light.
       scene.createDefaultCameraOrLight(true, true, true);
-
-      // The default camera looks at the back of the asset.
-      // Rotate the camera by 180 degrees to the front of the asset.
-      scene.activeCamera.alpha += Math.PI;
     }
   );
 
