17a2432f7921d3186ab4822d2f47d51bcdd80423
Simon Stucki <simon_stucki@hotmail.com>
2021-04-09 14:06:38 +0200

    Apply prettier

--- benchmark/helpers.js
+++ benchmark/helpers.js
@@ -1,40 +1,40 @@
-const fs = require('fs')
+const fs = require("fs");
 
 const startTimer = () => {
-  const start = process.hrtime.bigint()
+  const start = process.hrtime.bigint();
   return () => {
-    const end = process.hrtime.bigint(start)
-    return end - start
-  }
-}
+    const end = process.hrtime.bigint(start);
+    return end - start;
+  };
+};
 
 const waitFor = async (ms) => {
   await new Promise((resolve) => {
     setTimeout(() => {
-      resolve()
-    }, ms)
-  })
-}
+      resolve();
+    }, ms);
+  });
+};
 
 const startLogGroup = (group) => {
-  const stopTimer = startTimer()
-  console.log(group + ' started')
+  const stopTimer = startTimer();
+  console.log(group + " started");
 
   return () => {
-    const diff = stopTimer()
-    console.log(group + ' ended, duration: ' + diff + 'ns')
-  }
-}
+    const diff = stopTimer();
+    console.log(group + " ended, duration: " + diff + "ns");
+  };
+};
 
 const createFolderIfNotExist = (folder) => {
   if (!fs.existsSync(folder)) {
-    fs.mkdirSync(folder)
+    fs.mkdirSync(folder);
   }
-}
+};
 
 module.exports = {
   startTimer,
   waitFor,
   startLogGroup,
   createFolderIfNotExist,
-}
+};
