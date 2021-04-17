01e9f0b4a643546dc06b93cdee3ba51ba4859a94
sir-marc <sir-marc@users.noreply.github.com>
2021-04-17 12:25:53 +0000

    Deploying to gh-pages from @ kreativwebdesign/lode@da1f7d50eb0885fbcfbc5481c6b5bd4e24c8fcf0 🚀

--- src/gltf-loader.js
+++ src/gltf-loader.js
@@ -1,37 +1,16 @@
-import { GLTFLoader } from "../_snowpack/pkg/three/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "../_snowpack/pkg/three.js";
+import loadGltfAsync from "./async-gltf-loader.js";
 
-const loader = new GLTFLoader();
-
-export const loadGltf = (scene, gltfPath, position, scale) => {
-  let resolve = null;
-  const promise = new Promise((res) => {
-    resolve = res;
-  });
-  loader.load(
-    gltfPath,
-    // called when the resource is loaded
-    function (gltf) {
-      scene.add(gltf.scene);
-      gltf.scene.scale.set(...scale);
-      gltf.scene.position.set(...position);
-      resolve();
-    },
-    // called while loading is progressing
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    // called when loading has errors
-    function (error) {
-      console.log("An error happened");
-    }
-  );
-  return promise;
-};
-
-export const loadGltfs = (scene, gltfs) => {
-  return Promise.all(
-    gltfs.map((gltf) =>
-      loadGltf(scene, gltf.gltfPath, gltf.position, gltf.scale)
+export const getLod = async (levelCount, basePath, fileName) => {
+  const lod = new THREE.LOD();
+  const lodArtifacts = await Promise.all(
+    [...Array(levelCount).keys()].map((_, i) =>
+      loadGltfAsync(`${basePath}/${fileName}-lod-${i}/${fileName}.gltf`)
     )
   );
+
+  lodArtifacts.forEach((artifact, i) => {
+    lod.addLevel(artifact.scene, i * 20);
+  });
+  return lod;
 };
