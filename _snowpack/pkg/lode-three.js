686838a1eae73ca95b2631312a417596e5138d30
sir-marc <sir-marc@users.noreply.github.com>
2021-04-30 17:28:37 +0000

    Deploying to gh-pages from @ kreativwebdesign/lode@c076a40f84d94218d0a44059dce0de6c3d499617 🚀

--- _snowpack/pkg/lode-three.js
+++ _snowpack/pkg/lode-three.js
@@ -31,7 +31,7 @@ const loadModel = async ({
   const config = manifest[fileIdentifier];
   const lodArtifacts = await Promise.all(
     config.levels.map((_, i) => {
-      const filePath = `${relativePathToLodeOutputFolder}/${ModelBasePath}/${artifactName}-lod-${i}/${artifactName}.gltf`;
+      const filePath = `${relativePathToLodeOutputFolder}/${modelBasePath}/${artifactName}-lod-${i}/${artifactName}.gltf`;
       return loadGltfAsync(filePath);
     })
   );
