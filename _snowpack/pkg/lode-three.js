56e8925b9befa26f6b7528c8fb1b5dabf8dd792d
sir-marc <sir-marc@users.noreply.github.com>
2021-04-30 19:41:35 +0000

    Deploying to gh-pages from @ kreativwebdesign/lode@1bbe81fc021fd8395a0626ab6a6bfb0647d8e343 🚀

--- _snowpack/pkg/lode-three.js
+++ _snowpack/pkg/lode-three.js
@@ -45,7 +45,6 @@ const loadModel = async ({ artifactName, lodeContext }) => {
       return threshold;
     }),
   ];
-  console.log(thresholds);
   lodArtifacts.forEach((artifact, i) => {
     if (i === 0) {
       lod.addLevel(artifact.scene, 0);
