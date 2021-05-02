945b350e70f849664a047d8109cd8cc830cd86d3
Marc <marc.berli@windowslive.com>
2021-05-02 19:14:29 +0200

    Add global styles

--- lode-ui/src/App.js
+++ lode-ui/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { ChakraProvider, Grid, GridItem } from "@chakra-ui/react";
+import { ChakraProvider, GlobalStyle, Grid, GridItem } from "@chakra-ui/react";
 import ModelChooser from "./ModelChooser";
 import ModelViewer from "./ModelViewer";
+import theme from "./theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>
         <Grid h="100%" templateColumns="200px 1fr" templateRows="1fr">
           <GridItem borderWidth={1} rowSpan={2}>
