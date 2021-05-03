5662c216719df490cf50213d6a3e9ec2e43fddf1
Marc <marc.berli@windowslive.com>
2021-05-03 22:07:11 +0200

    Add updateModel api call

--- cli/lib/server/index.js
+++ cli/lib/server/index.js
@@ -1,24 +1,40 @@
+import glob from "glob";
 import path from "path";
 import sirv from "sirv";
 import { App } from "@tinyhttp/app";
-import { readFile } from "../helper/files.js";
-import { MANIFEST_FILENAME } from "../constants.js";
+import { json } from "milliparsec";
+import { cors } from "@tinyhttp/cors";
+import { readFile, writeFile } from "../helper/files.js";
+import { CONFIG_FILENAME, MANIFEST_FILENAME } from "../constants.js";
+import buildManifest from "../app/run/buildManifest.js";
 
-export const startServer = ({ outputFoldername }) => {
+export const startServer = (opts) => {
   const app = new App();
 
   app
-    .use(function someMiddleware(req, res, next) {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      next();
-    })
-    .use("/assets", sirv(outputFoldername))
+    .use(json())
+    .use(cors({ origin: "*", allowedHeaders: "*" }))
+    .options("*", cors())
+    .use("/assets", sirv(opts.outputFoldername))
     .get("/", (_, res) => {
       res.send("<h1>lode API is running</h1>");
     })
     .get("/manifest", (req, res) => {
-      const file = readFile(path.join(outputFoldername, MANIFEST_FILENAME));
+      const file = readFile(
+        path.join(opts.outputFoldername, MANIFEST_FILENAME)
+      );
       res.status(200).send(file);
+    })
+    .post("/updateModel", async (req, res) => {
+      const { name, model } = req.body;
+      writeFile(
+        path.join(name, CONFIG_FILENAME),
+        JSON.stringify(model, null, 2)
+      );
+      const sourceFiles = glob.sync(opts.source);
+      const manifest = buildManifest(opts.outputFoldername, sourceFiles);
+
+      res.status(200).send(manifest);
     })
     .listen(3001);
 
