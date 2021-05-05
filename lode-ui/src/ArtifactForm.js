64434562b0309c765b0145444a24c2cacd5a70dc
Marc <marc.berli@windowslive.com>
2021-05-05 13:08:25 +0200

    Add targetScale to artifact form

--- lode-ui/src/ArtifactForm.js
+++ lode-ui/src/ArtifactForm.js
@@ -1,17 +1,28 @@
 import { Box, Flex } from "@chakra-ui/layout";
 import { Input } from "@chakra-ui/input";
 import { useEffect, useState } from "react";
+import deepEql from "deep-eql";
+import { Button } from "@chakra-ui/button";
 
 function ArtifactForm({ level, updateArtifact }) {
   const { distance, ...l } = level;
-  const [threshold, setThreshold] = useState(l.threshold);
+  const [levelState, setLevel] = useState(l);
   useEffect(() => {
-    setThreshold(l.threshold);
-  }, [l.threshold]);
-  const onChange = (e) => setThreshold(parseInt(e.target.value));
+    setLevel(l);
+  }, [deepEql(l)]);
+  const onThresholdChange = (e) =>
+    setLevel((l) => ({ ...l, threshold: parseInt(e.target.value) }));
+  const onTargetScaleChange = (e) =>
+    setLevel((l) => ({
+      ...l,
+      configuration: {
+        ...l.configuration,
+        targetScale: parseFloat(e.target.value),
+      },
+    }));
   const onSubmit = (e) => {
     e.preventDefault();
-    updateArtifact({ ...l, threshold });
+    updateArtifact(levelState);
   };
   return (
     <Box as="form" onSubmit={onSubmit}>
@@ -19,9 +30,31 @@ function ArtifactForm({ level, updateArtifact }) {
         <Box flexShrink={0} mr={2}>
           Trigger at distance:
         </Box>
-        <Input value={threshold} type="number" onChange={onChange} />
+        <Input
+          value={levelState.threshold}
+          type="number"
+          onChange={onThresholdChange}
+        />
       </Flex>
-      (Visible from: {distance - l.threshold}-{distance})
+      {!!levelState.configuration && (
+        <Flex as="label" align="center">
+          <Box flexShrink={0} mr={2}>
+            Targetscale:
+          </Box>
+          <Input
+            value={levelState.configuration.targetScale}
+            type="number"
+            onChange={onTargetScaleChange}
+            min={0}
+            max={1}
+            step={0.00001}
+          />
+        </Flex>
+      )}
+      <Flex as="label" align="center" justify="space-between">
+        (Visible from: {distance - l.threshold}-{distance})
+        <Button type="submit">Speichern</Button>
+      </Flex>
     </Box>
   );
 }
