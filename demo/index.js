6bf0041a2c475b7ccef701d956e44ac9619fd06a
Marc <marc.berli@windowslive.com>
2021-05-11 19:03:29 +0200

    Add seedrandom to always generate the same scene

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { measureFPS } from "./src/measure-fps";
 import loadGltfAsync from "./src/async-gltf-loader";
 import generateRandomPosition from "./src/generate-random-position";
