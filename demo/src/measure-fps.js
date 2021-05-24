92b514012455868d73c40d88e2a8ee9fe8c1e013
Marc <marc.berli@windowslive.com>
2021-05-24 17:04:08 +0200

    Check mark instead of try and catch

--- demo/src/measure-fps.js
+++ demo/src/measure-fps.js
@@ -1,7 +1,13 @@
 const markerName = "lastFrame";
 const measureName = "time since last frame";
+
+const isFirstRun = () => !performance.getEntriesByName(markerName)[0];
+
 export function measureFPS() {
-  try {
+  if (isFirstRun()) {
+    performance.mark(markerName);
+    return 0;
+  } else {
     performance.measure(measureName, markerName);
     const performanceEntry = performance.getEntriesByName(measureName)[0];
     const timeSinceLastFrame = performanceEntry.duration;
@@ -9,8 +15,5 @@ export function measureFPS() {
     performance.mark(markerName);
     const FPS = 1 / (timeSinceLastFrame / 1000);
     console.log("::benchmark::fps::" + FPS);
-  } catch (e) {
-    performance.mark(markerName);
-    return 0;
   }
 }
