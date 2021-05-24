2739c58571188e24955c0f5e06e377fc07f57544
sir-marc <sir-marc@users.noreply.github.com>
2021-05-24 15:06:33 +0000

    Deploying to gh-pages from @ kreativwebdesign/lode@e975567910d1616167260e535e536f4329d90f86 🚀

--- src/measure-fps.js
+++ src/measure-fps.js
@@ -1,11 +1,19 @@
 const markerName = "lastFrame";
 const measureName = "time since last frame";
-performance.mark(markerName);
+
+const isFirstRun = () => !performance.getEntriesByName(markerName)[0];
+
 export function measureFPS() {
-  performance.measure(measureName, markerName);
-  const performanceEntry = performance.getEntriesByName(measureName)[0];
-  const timeSinceLastFrame = performanceEntry.duration;
-  performance.clearMeasures(measureName);
-  performance.mark(markerName);
-  return 1 / (timeSinceLastFrame / 1000);
+  if (isFirstRun()) {
+    performance.mark(markerName);
+    return 0;
+  } else {
+    performance.measure(measureName, markerName);
+    const performanceEntry = performance.getEntriesByName(measureName)[0];
+    const timeSinceLastFrame = performanceEntry.duration;
+    performance.clearMeasures(measureName);
+    performance.mark(markerName);
+    const FPS = 1 / (timeSinceLastFrame / 1000);
+    console.log("::benchmark::fps::" + FPS);
+  }
 }
