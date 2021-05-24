71ccb80cb9aeaf57ea378741296bd31aca41b4aa
Marc Berli <marc.berli@windowslive.com>
2021-05-24 17:57:55 +0200

    Fix initial FPS calculation (#155)
    Co-authored-by: Simon Stucki <simon_stucki@hotmail.com>

--- demo/src/measure-fps.js
+++ demo/src/measure-fps.js
@@ -1,11 +1,19 @@
 const markerName = "lastFrame";
 const measureName = "time since last frame";
-performance.mark(markerName);
-export function measureFPS() {
-  performance.measure(measureName, markerName);
-  const performanceEntry = performance.getEntriesByName(measureName)[0];
-  const timeSinceLastFrame = performanceEntry.duration;
-  performance.clearMeasures(measureName);
-  performance.mark(markerName);
-  return 1 / (timeSinceLastFrame / 1000);
+
+export function buildFpsMeasurement() {
+  let isFirstRun = true;
+  return () => {
+    if (isFirstRun) {
+      performance.mark(markerName);
+      isFirstRun = false;
+      return undefined;
+    }
+    performance.measure(measureName, markerName);
+    const performanceEntry = performance.getEntriesByName(measureName)[0];
+    const timeSinceLastFrame = performanceEntry.duration;
+    performance.clearMeasures(measureName);
+    performance.mark(markerName);
+    return 1 / (timeSinceLastFrame / 1000);
+  };
 }
