ae065672471601644798c661558ba0767e01bd63
Simon Stucki <simon_stucki@hotmail.com>
2021-06-05 13:55:07 +0200

    Refactor error calcuation method

--- cli/lib/simplification/error-calculation.js
+++ cli/lib/simplification/error-calculation.js
@@ -6,6 +6,19 @@ export const calculateVertexError = (q, point) => {
   const y = point[1];
   const z = point[2];
   const m = q.data;
+  /**
+   * Source: https://www.cs.cmu.edu/~./garland/Papers/quadrics.pdf (footnote 2)
+   * 1 * q11 * x^2 +
+   * 2 * q12 * x * y +
+   * 2 * q13 * x * z +
+   * 2 * q14 * x +
+   * 1 * q22 * y^2 +
+   * 2 * q23 * y * z +
+   * 2 * q24 * y +
+   * 2 * q33 * z^2 +
+   * 2 * q34 * z +
+   * q44
+   */
   return (
     m[0] * x * x +
     2 * m[1] * x * y +
@@ -27,16 +40,16 @@ export const calculateVertexError = (q, point) => {
  * @returns
  */
 const findOptimalVertexPosition = (qDelta, det) => {
-  const resultPoint = vec3.create();
   // x = A41 / det(qDelta)
-  resultPoint[0] = (-1 / det) * qDelta.det(1, 2, 3, 4, 5, 6, 5, 7, 8);
+  const x = (-1 / det) * qDelta.det(1, 2, 3, 4, 5, 6, 5, 7, 8);
 
   // y = A42 / det(qDelta)
-  resultPoint[1] = (1 / det) * qDelta.det(0, 2, 3, 1, 5, 6, 2, 7, 8);
+  const y = (1 / det) * qDelta.det(0, 2, 3, 1, 5, 6, 2, 7, 8);
 
   // z = A43 / det(qDelta)
-  resultPoint[2] = (-1 / det) * qDelta.det(0, 1, 3, 1, 4, 6, 2, 5, 8);
+  const z = (-1 / det) * qDelta.det(0, 1, 3, 1, 4, 6, 2, 5, 8);
 
+  const resultPoint = vec3.fromValues(x, y, z);
   const error = calculateVertexError(qDelta, resultPoint);
   return { error, point: resultPoint };
 };
@@ -70,14 +83,14 @@ export const calculateError = (vertex1, vertex2) => {
   // determinant is used to check if matrix is invertible
   const det = qDelta.det(0, 1, 2, 1, 4, 5, 2, 5, 7);
 
-  if (det != 0 && !isBorder) {
-    // matrix is invertible, we can therefore look for the optimal solution
-    return findOptimalVertexPosition(qDelta, det);
-  } else {
+  if (det === 0 || isBorder) {
     // determinant is zero, therefore it's not possible to take optimal solution
     // for borders it's often an issue with creases, in these cases the simple approach delivers more appropriate results
     return takeSimpleVertexPosition(vertex1, vertex2, qDelta);
   }
+
+  // matrix is invertible, we can therefore look for the optimal solution
+  return findOptimalVertexPosition(qDelta, det);
 };
 
 export const calculateTriangleError = (triangle, vertices) => {
